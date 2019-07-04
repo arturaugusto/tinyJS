dce5f8a27ba8c045f343baae9fad5c7ae53c74f3
Artur <arturaugusto@gmail.com>
2019-07-04 13:45:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,51 @@
 var _ = function(value) {
-  this.value = value;
+  this._value = value
   this.groupBy = (...args) => {
     // arg
-    var arr, fn;
-    if (this.value !== undefined) {
-      arr = this.value;
-      fn = args[0];
+    var arr, fn
+    if (this._value !== undefined) {
+      arr = this._value
+      fn = args[0]
     } else {
-      arr = args[0];
-      fn = args[1];
+      arr = args[0]
+      fn = args[1]
     }
     // logic
     var res = arr.reduce((acc, curr, i, arr) => {
-      let r = fn(curr, i, arr);
+      let r = fn(curr, i, arr)
       if(!acc[r]){
         acc[r] = []
-      };
-      acc[r].push(curr);
-      return acc;
-    }, {});
-    console.log(res)
+      }
+      acc[r].push(curr)
+      return acc
+    }, {})
     // return
-    if (this.value !== undefined) {
-      this.value = res
+    if (this._value !== undefined) {
+      this._value = res
       return this
     } else {
       return res
     }
   }
+
+  // End chain
+  this.value = () => {
+    var res = this._value
+    this._value = undefined
+    return res
+  }
+
+  ['map', 'filter', 'reduce'].map((fnName) => {
+    this[fnName] = (...args) => {
+      if (this._value === undefined) {
+        console.error('Method only avaliable for chaining.')
+        return
+      }
+      var res = arr[fnName](...args)
+      this._value = res
+      return this
+    }
+  })
 }
 
-x = new _([1,2,3,4,5]).groupBy((x) => x%2)
+x = new _([1,2,3,4,5]).filter((x) => x < 5).groupBy((x) => x%2).value()
