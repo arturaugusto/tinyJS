90517de632efad10b0b0b36ba1021bbfb04d72e3
Artur <arturaugusto@gmail.com>
2019-07-04 13:23:47 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-var groupBy = (arr, fn) => arr.reduce((acc, c) => {let r = fn(c);if(!acc[r]){acc[r] = []};acc[r].push(c);return acc;}, {})
+var _ = function(value) {
+  this.value = value;
+  this.groupBy = (...args) => {
+    // arg
+    var arr, fn;
+    if (this.value !== undefined) {
+      arr = this.value;
+      fn = args[0];
+    } else {
+      arr = args[0];
+      fn = args[1];
+    }
+    // logic
+    var res = arr.reduce((acc, curr, i, arr) => {
+      let r = fn(curr, i, arr);
+      if(!acc[r]){
+        acc[r] = []
+      };
+      acc[r].push(curr);
+      return acc;
+    }, {});
+    console.log(res)
+    // return
+    if (this.value !== undefined) {
+      this.value = res
+      return this
+    } else {
+      return res
+    }
+  }
+}
+
+x = new _([1,2,3,4,5]).groupBy((x) => x%2)
